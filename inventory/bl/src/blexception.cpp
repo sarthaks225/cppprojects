dcc64af575e1f47c787d14a922bed0aa790bd2ab
Sarthak Sharma <sarthaks225@gmail.com>
2022-03-21 19:34:28 +0530

    *tmcommon folder created for common codes (stringutils added),*bl foldercreated contains(include,src,lib,testcaes) header files created in bl (blexception,iuom,uom) and src of bl created(blexception.cpp,UnitOfMeasurement.cpp), include guard name changed for dl (header files)

---
+++ inventory/bl/src/blexception.cpp
@@ -0,0 +1,72 @@
+#include<blexception>
+using namespace inventory; 
+using namespace business_layer; 
+
+BLException::BLException()
+{
+this->genericException="";
+}
+BLException::BLException(string genericException)
+{
+this->genericException=genericException;
+}
+BLException::BLException(const BLException &other)
+{
+this->genericException=other.genericException;
+}
+BLException::~BLException() throw()
+{
+// do nothing
+}
+BLException & BLException::operator=(const BLException &other)
+{
+this->genericException=other.genericException;
+return *this;
+}
+const char * BLException::what() const throw()
+{
+return this->genericException.c_str();
+}
+
+int BLException::hasGenericException()
+{
+return this->genericException.length()>0;
+}
+void BLException::setGenericException(string genericException)
+{
+this->genericException=genericException;
+}
+string BLException::getGenericException()
+{
+return this->genericException;
+}
+void BLException::addPropertyException(string property,string exception)
+{
+this->exceptions.insert(pair<string,string>(property,exception));
+}
+string BLException::getPropertyException(string property)
+{
+map<string,string>::iterator i;
+i=this->exceptions.find(property);
+if(i==this->exceptions.end())
+{
+return string("");
+}
+return (*i).second;
+}
+int BLException::hasPropertyException(string property)
+{
+return this->getPropertyException(property).length()>0;
+}
+int BLException::hasPropertyExceptions()
+{
+return this->getPropertyExceptionCount();
+}
+int BLException::hasExceptions()
+{
+return this->genericException.length()>0 || this->exceptions.size()>0;
+}
+int BLException::getPropertyExceptionCount()
+{
+return this->exceptions.size();
+}
